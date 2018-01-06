b19d07674a5b1d4fa356c0e0700735958418c800
webstar1987923 <webstar1987923@hotmail.com>
2018-01-06 21:05:24 +0300

    all

---
+++ includes/header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8; image/svg+xml">
+	<title>Soundrus</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+   <script src="assets/js/jquery.js"></script>
+   <script src="vendors/slider/jquery.fractionslider.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
+   <link rel="stylesheet" type="text/css" href="vendors/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="vendors/bootstrap/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="vendors/slider/css/fractionslider.css">
+	<link rel="stylesheet" type="text/css" href="vendors/fonts/fonts-pe-icon/css/pe-icon-set-media.min.css">
+	<link rel="stylesheet" type="text/css" href="vendors/fonts/linear-icon/style.css">
+	<link rel="stylesheet" type="text/css" href="http://redhawk-studio.com/demos/rizwan/remix/PSD/assets/css/style.css">
+</head>
+<body>
+	<header class="main-menu">
+		<div class="container">
+			<div class="row align-items-center">
+			<div class="col-md-12">
+				<nav class="navbar navbar-expand-lg navbar-dark">
+  					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#left_menu" aria-controls="left_menu" aria-expanded="false" aria-label="Toggle navigation">
+    					<i class="fa fa-align-justify" aria-hidden="true"></i>
+  					</button>
+					<div class="collapse navbar-collapse" id="left_menu">
+    					<ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="navbar-brand nav-link" href="#">Site Logo</a>
+                </li>
+      						<li class="nav-item active">
+        						<a class="nav-link" href="#">Home</a>
+      						</li>
+      						<li class="nav-item">
+        						<a class="nav-link" href="#">Looks</a>
+      						</li>  
+      						<li class="nav-item">
+        						<a class="nav-link" href="#">Eat Well</a>
+      						</li> 
+      						
+  							<li class="nav-item">
+        						<a class="nav-link" href="#">Sports Life</a>
+      						</li>
+      						<li class="nav-item">
+        						<a class="nav-link" href="#">Memories</a>
+      						</li>
+      						<li class="nav-item">
+        						<a class="nav-link" href="#">Man Stuff</a>
+      						</li> 
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Vintage Stuff</a>
+                  </li> 
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Counterculture</a>
+                  </li>      
+    					</ul>
+  					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+	</header>
+   <div class="slider-wrapper">
+         <div class="responisve-container">
+            <div class="slider">
+               <div class="fs_loader"></div>
+               <div class="slide">                       
+                  <img src="assets/images/slide_bg.jpg" style="width: 100%;">
+                  
+                  <h1 class="claim light-green"        
+                        data-position="100,150" data-in="top" data-step="1" data-out="top" data-ease-in="easeOutBounce">Welcome to our Website</h1>
+
+                  <h1 class="claim light-green"        
+                        data-position="150,180" data-in="bottom" data-step="2" data-out="top" data-ease-in="easeOutBounce">Loram lupsum Loram lupsumLoram</h1>
+                 
+               </div>
+               
+            </div>
+         </div>
+      </div>
---
+++ index.php
@@ -0,0 +1,190 @@
+<?php include 'includes/header.php'; ?>
+
+<content class="page_content">
+		<div class="slider_bottom row">
+			<div class="col-lg-4 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti1.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h2>Título da Publicação</h2>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut 
+dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie 
+imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti2.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h2>Título da Publicação</h2>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti3.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h2>Título da Publicação</h2>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<section id="about">
+			<div class="row">
+				<div class="col-lg-2">
+					<img src="assets/images/noti3.jpg" class="rounded-circle" width="100%">
+				</div>
+				<div class="col-lg-10">
+					<h1>#AUTOR</h1>
+					<hr style="width: 30%;border-color: #fff; margin: 0;margin-bottom:20px;">
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie imperdiet. Ut vel ipsum bibendum, 
+facilisis magna sed, auctor metus. Vestibulum porta tincidunt est, non mollis sem gravida ullamcorper. Fusce volutpat metus vel metus mattis placerat. In varius fermentum 
+ornare. Morbi eu turpis eget turpis vulputate semper a vitae eros. Suspendisse cursus pharetra mi, ac rutrum nisi suscipit at. Curabitur orci sem, vestibulum sit amet v
+estibulum vel, fringilla ac magna. In et est suscipit, elementum odio vel, pharetra ligula. Praesent non varius tortor.</p>
+					<div class="social_icons">
+						<a href="#" class="social_icon_item"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						<a href="#" class="social_icon_item"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			</div>
+			<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+		</section>
+		<div class="slider_bottom row back-gray">
+			<div class="col-lg-3 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti1.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h5>Título da Publicação</h5>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut 
+dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie 
+imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti2.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h5>Título da Publicação</h5>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti3.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h5>Título da Publicação</h5>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 noti_item padding-5">
+				<div class="image_container">
+					<img src="assets/images/noti3.jpg" width="100%">
+				</div>
+				<div class="noti_content">
+					<div class="row">
+					<div class="col-lg-10">
+					<h5>Título da Publicação</h5>
+					<hr>
+					<p>Ut laoreet elementum turpis vitae finibus. Fusce risus enim, dictum ut dui nec, tincidunt dictum augue. Sed vestibulum nunc vitae molestie imperdiet. Ut vel ipsum</p>
+						</div>
+						<div class="col-lg-2 align-middle">
+							<a href="#" class="icon"><span class=" lnr lnr-plus-circle"></span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12 link_arrow">
+			<div class="float-right"><a href="#" class="link_wit_arrow">Publicações Anteriores</a></div>
+		</div>
+		</div>
+		<section id="map">
+			<div class="row">
+				<div class="col-lg-9 padding-0 margin-0">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d34994576.84189522!2d157.6460847707271!3d-25.798941516900335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2b3628c340e4b37d%3A0x40217a82a254560!2sAnmatjere+NT+0872%2C+Australia!5e0!3m2!1sen!2s!4v1510058934467" width="100%" height="397" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<div class="col-lg-3 noti_item padding-0 margin-0 map-side">
+				<div class="image_container" style="overflow: hidden;">
+					<img src="assets/images/map-side.png" height="100%" class="float-right">
+				</div>
+				<h4>#Locais Visitados</h4>
+				
+			</div>
+				</div>
+		</section>
+		<section id="subscribe">
+				<div class="subscribe_container">
+					<div class="row">
+					<div class="col-lg-3 padding-0 margin-0">
+						<h5>Fique a par das novidades:</h5>
+					</div>
+					<div class="col-lg-6 padding-0 margin-0">
+						<input type="text" style="width:100%">
+					</div>
+					<div class="col-lg-2">
+						<button class="subs_button">SUBSCREVER</button>
+					</div>
+					</div>
+				</div>
+		</section>
+</content>
+
+
+<?php include 'includes/footer.php'; ?>
+<script type="text/javascript">
+	$(".custom-seekbar").on("click", function(e){
+    var offset = $(this).offset();
+    console.log(offset);
+});
+</script>
